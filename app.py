8ee40ae4b939c7c503a689a500b1398893759560
Tobias Rasmussen <54395197+RasmussenTobias@users.noreply.github.com>
2023-06-09 01:30:29 +0200

    Added a custom split function using jinja2

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from connectionAuth import conn
 import psycopg2
 from jinja2 import pass_eval_context
 from markupsafe import Markup, escape
+from flask import session
+import secrets
 cur = conn.cursor()
 app = Flask(__name__)
+secret_key = secrets.token_hex(16)
+app.secret_key = secret_key
 
 liga = None
 
@@ -48,12 +52,16 @@ def welcome():
        return render_template("ligaPrev.html")
     else:
        return redirect(url_for("createUser"))
-    
+ 
+#Custom split function    
 @app.template_filter('custom_split')
 @pass_eval_context
 def custom_split(eval_ctx, value, index):
     parts = value.split('_')
-    return Markup.escape(parts[index]) if eval_ctx.autoescape else parts[index]
+    if index < len(parts):
+        return Markup.escape(parts[index]) if eval_ctx.autoescape else parts[index]
+    else:
+        return ""
 #preview
     
 @app.route("/ligaPreview",methods=["POST"])
@@ -66,30 +74,35 @@ def showMatches():
    
    return render_template("seasons.html",num_buttons=len(seasonsForLiga),button_desc=description,liga=liga)
 
-@app.route("/matches",methods=["post"])
+@app.route("/matches", methods=["POST"])
 def matches():
-   
-   cur.execute('''
-               with tb1 as
-               (select date,playingTeams,max(home) as h,max(draw) as d,max(away) as a
-               from odds group by playingTeams,date limit 100)
-               select date,playingTeams,(1/h+1/d+1/a)*100-100 as arb from tb1
-               ''')
-   res = cur.fetchall()
-   
-   return render_template("matches.html",rows = res,
-                          totalMatches=len(res),
-                          liga=liga)
+    cur.execute('''
+        WITH tb1 AS (
+            SELECT date, playingTeams, MAX(home) AS h, MAX(draw) AS d, MAX(away) AS a
+            FROM odds GROUP BY playingTeams, date LIMIT 100
+        )
+        SELECT date, playingTeams, (1 / h + 1 / d + 1 / a) * 100 - 100 AS arb FROM tb1
+    ''')
+    res = cur.fetchall()
+
+    session['rows'] = res  # Store rows in the session
+
+    return render_template("matches.html", rows=res, totalMatches=len(res), liga=liga)
                           
 
-@app.route("/matchStats",methods=["GET"])
+@app.route("/matchStats", methods=["POST"])
 def showMatchStats():
-   key = request.form["button"]
-   cur.execute("select * from matchStats where datePlayed AND playingTeams =%s",(key.split("*")[0],key.split("*")[1]))
-   stats = cur.fetchone()
-   #return print(test)
-   return render_template("singleMatch.html",match_stats = stats) 
-
+    index = int(request.form.get("click"))
+    rows = session.get('rows')  # Retrieve rows from the session
+    if rows and 0 <= index < len(rows):
+        row = rows[index]
+        played_date = row[0]
+        playing_teams = row[1]
+        cur.execute("SELECT * FROM matchStats WHERE datePlayed = %s AND playingTeams = %s", (played_date, playing_teams))
+        match_stats = cur.fetchone()
+        print(match_stats)
+    return render_template("singleMatch.html", match_stats=match_stats)
+   
 
 if __name__ == '__main__':
     app.debug = True
